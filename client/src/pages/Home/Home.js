2a94165fc8e0516629a91ab0a17243dbc5d0ce48
jyanglau88 <jyanglau88@gmail.com>
2018-11-24 21:46:01 -0800

    updates

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,12 +3,20 @@ import Jumbotron from "../../components/Jumbotron";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 
-<Container fluid>
-<Row>
-  <Col size="md-3">
-    <Jumbotron>
-    </Jumbotron>
+class Contact extends Component {
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbotron>
+                            Welcome!
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
 
-          </Col>
-        </Row>
-      </Container>
+export default Contact;
